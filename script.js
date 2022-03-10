117558517688f82ece8d518f4b31a4a635dacad1
Zaigham Salehi <zsalehi313@gmail.com>
2022-03-10 17:53:38 -0500

    Update function makeGrid, apply minimal css styling

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const squares = document.querySelector(`.sketchpad`);
-let gridSize = 0;
+//const askGrid = 
+let gridSize = 16 * 16;
 
 function makeGrid () {
     for (i = 0; i <= gridSize; i++) {
         const newSquare = document.createElement('div');
         newSquare.classList.add('square');
-        squares.appendChild('newSquare');
+        newSquare.style.border = "1px solid black";
+        squares.appendChild(newSquare);
     }
 
 }
+
+makeGrid();
 
