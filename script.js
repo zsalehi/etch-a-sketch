3458dd7f860e2f6b8d9e9b62130de24efa2c7f9b
Zaigham Salehi <zsalehi313@gmail.com>
2022-03-10 18:57:20 -0500

    Fixed makeGrid, add Event listeners to each square

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 const squares = document.querySelector(`.sketchpad`);
 //const askGrid = 
-let gridSize = 16 * 16;
+let gridSize = 16;
 
-function makeGrid () {
-    for (i = 0; i <= gridSize; i++) {
-        const newSquare = document.createElement('div');
-        newSquare.classList.add('square');
-        newSquare.style.border = "1px solid black";
-        squares.appendChild(newSquare);
-    }
+function makeGrid (size) {
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        squares.appendChild(row);
+        console.log(i);
+        for (let x = 0; x < size; x++) {
+            const newSquare = document.createElement('div');
+            newSquare.classList.add('square');
+            //newSquare.style.border = "1px solid black";
+            row.appendChild(newSquare);
+            let height =  960 / size;
+            newSquare.style.height = `${height}px`;
+        }
+    } 
+    
+    const allSquares = document.querySelectorAll('.square');
+    allSquares.forEach( item => {
+        item.addEventListener('mouseover', () => {
+            item.classList.add('hover');
+        })
+    })
+
+    
 
 }
 
-makeGrid();
+makeGrid(gridSize);
 
